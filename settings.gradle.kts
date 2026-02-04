4fd3afe8b46acc50a454c5fa25f012aff0b5b700
jch0029987-glitch <jch0029987@gmail.com>
2026-02-03 19:25:53 -0600

    fix: add google repo to pluginManagement and upgrade to Gradle 9.1 for AGP 9.0 support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,19 @@
+pluginManagement {
+    repositories {
+        google()            // <--- Add this
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") } 
-        // Optional fallback if jitpack.io times out:
-        // maven { url = uri("https://javadoc.jitpack.io") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
+
+rootProject.name = "PipeTV"
+include(":app")
+
